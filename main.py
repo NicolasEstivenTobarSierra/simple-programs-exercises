54da1fb65fb3b385c5872357444304f0b73b3053
NicolasEstivenTobarSierra <186078984+NicolasEstivenTobarSierra@users.noreply.github.com>
2024-10-29 16:36:04 -0500

    ejercicio 5.1

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # Solicitar al usuario que ingrese un entero de tres dígitos
-numero = input("Ingresa un número entero de tres dígitos: ")
+number = input("Enter a three-digit integer: ")
 
 # Verificar que el número tenga exactamente tres dígitos
-if len(numero) == 3 and numero.isdigit():
+if len(number) == 3 and number.isdigit():
     # Invertir los dígitos
-    numero_invertido = numero[2] + numero[1] + numero[0]
+    numero_invertido = number[2] + number[1] + number[0]
     
     # Mostrar el resultado
-    print("El número con los dígitos en orden inverso es:", numero_invertido)
+    print("The number with the digits in reverse order is:", numero_invertido)
 else:
-    print("Por favor, asegúrate de ingresar un número entero de tres dígitos.")
+    print("Please, make sure to enter a three-digit number")
