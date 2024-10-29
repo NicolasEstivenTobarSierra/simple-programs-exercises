7ba36b03f53a08be5c3f200cc7b190a6d3b0d091
NicolasEstivenTobarSierra <186078984+NicolasEstivenTobarSierra@users.noreply.github.com>
2024-10-29 16:24:36 -0500

    ejercicio 4.0

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-# Pedir al usuario que ingrese 4 notas
-note1 = float(input("Enter the first note: "))  # Primera nota
-note2 = float(input("Enter the second note: "))  # Segunda nota
-note3 = float(input("Enter the third note: "))   # Tercera nota
-note4 = float(input("Enter the quarter note: "))    # Cuarta nota
+# Solicitar al usuario que ingrese la medida en centímetros
+centimetros = float(input("Ingresa la medida en centímetros: "))
 
-# Calcular el promedio
-promedio = (note1 + note2 + note3 + note4) / 4  # Sumar las notas y dividir entre 4
+# Realizar la conversión
+pulgadas = centimetros / 2.54
 
 # Mostrar el resultado
-print("El promedio es:", promedio)  # Imprimir el promedio
+print(f"{centimetros} centímetros son {pulgadas} pulgadas.")
